456566b1a209855ed1d857ad8edf3618500f8202
Johanna Osterrieter <info@johannaost.de>
2021-11-14 22:08:40 +0100

    stability for script.js

--- public/script.js
+++ public/script.js
@@ -1,36 +1,40 @@
 (function () {
-    const canvas = document.getElementById("canvas");
-    const ctx = canvas.getContext("2d");
-    let isPressed = false;
-    const signatureVal = document.getElementById("signature");
-    const offsetLeft = canvas.getBoundingClientRect().left + window.pageXOffset;
-    const offsetTop = canvas.getBoundingClientRect().top + window.pageYOffset;
+    window.onload = () => {
+        const canvas = document.getElementById("canvas");
+        const ctx = canvas.getContext("2d");
+        let isPressed = false;
+        const signatureVal = document.getElementById("signature");
+        const offsetLeft =
+            canvas.getBoundingClientRect().left + window.pageXOffset;
+        const offsetTop =
+            canvas.getBoundingClientRect().top + window.pageYOffset;
 
-    ctx.lineCap = "round";
-    ctx.lineJoin = "round";
-    ctx.strokeStyle = "black";
-    ctx.lineWidth = "1";
+        ctx.lineCap = "round";
+        ctx.lineJoin = "round";
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = "1";
 
-    canvas.addEventListener("mousedown", (event) => {
-        console.log(event.pageY - offsetTop);
-        isPressed = true;
-        ctx.beginPath();
-        ctx.moveTo(event.pageX - offsetLeft, event.pageY - offsetTop);
-    });
+        canvas.addEventListener("mousedown", (event) => {
+            console.log(event.pageY - offsetTop);
+            isPressed = true;
+            ctx.beginPath();
+            ctx.moveTo(event.pageX - offsetLeft, event.pageY - offsetTop);
+        });
 
-    document.addEventListener("mouseup", () => {
-        if (isPressed) {
-            const signatureUrl = canvas.toDataURL();
-            signatureVal.setAttribute("value", signatureUrl);
-        }
-        isPressed = false;
-    });
+        document.addEventListener("mouseup", () => {
+            if (isPressed) {
+                const signatureUrl = canvas.toDataURL();
+                signatureVal.setAttribute("value", signatureUrl);
+            }
+            isPressed = false;
+        });
 
-    canvas.addEventListener("mousemove", (event) => {
-        if (isPressed) {
-            // start drawing
-            ctx.lineTo(event.pageX - offsetLeft, event.pageY - offsetTop);
-            ctx.stroke();
-        }
-    });
+        canvas.addEventListener("mousemove", (event) => {
+            if (isPressed) {
+                // start drawing
+                ctx.lineTo(event.pageX - offsetLeft, event.pageY - offsetTop);
+                ctx.stroke();
+            }
+        });
+    };
 })();
