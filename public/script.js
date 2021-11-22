64de2a5798e8aa08bbbc4f1fd1c9da11eeb5c714
Johanna Osterrieter <info@johannaost.de>
2021-11-22 13:26:22 +0100

    added draw on touch screens

--- public/script.js
+++ public/script.js
@@ -9,32 +9,58 @@
         const offsetTop =
             canvas.getBoundingClientRect().top + window.pageYOffset;
 
+        let mouseX = null;
+        let mouseY = null;
+
         ctx.lineCap = "round";
         ctx.lineJoin = "round";
         ctx.strokeStyle = "black";
         ctx.lineWidth = "1";
 
-        canvas.addEventListener("mousedown", (event) => {
-            console.log(event.pageY - offsetTop);
+        function pressed(event) {
+            if (event.type == "touchstart") {
+                mouseX = event.touches[0].pageX - offsetLeft;
+                mouseY = event.touches[0].pageY - offsetTop;
+            } else {
+                mouseX = event.pageX - offsetLeft;
+                mouseY = event.pageY - offsetTop;
+            }
+
             isPressed = true;
             ctx.beginPath();
-            ctx.moveTo(event.pageX - offsetLeft, event.pageY - offsetTop);
-        });
+            ctx.moveTo(mouseX, mouseY);
+        }
 
-        document.addEventListener("mouseup", () => {
+        function released() {
+            console.log("released!");
             if (isPressed) {
                 const signatureUrl = canvas.toDataURL();
                 signatureVal.setAttribute("value", signatureUrl);
             }
             isPressed = false;
-        });
+        }
 
-        canvas.addEventListener("mousemove", (event) => {
+        function moving(event) {
             if (isPressed) {
+                if (event.type == "touchmove") {
+                    mouseX = event.touches[0].pageX - offsetLeft;
+                    mouseY = event.touches[0].pageY - offsetTop;
+                } else {
+                    mouseX = event.pageX - offsetLeft;
+                    mouseY = event.pageY - offsetTop;
+                }
                 // start drawing
-                ctx.lineTo(event.pageX - offsetLeft, event.pageY - offsetTop);
+                ctx.lineTo(mouseX, mouseY);
                 ctx.stroke();
             }
-        });
+        }
+
+        canvas.addEventListener("mousedown", pressed);
+        document.addEventListener("mouseup", released);
+        canvas.addEventListener("mousemove", moving);
+
+        canvas.addEventListener("touchstart", pressed);
+        document.addEventListener("touchend", released);
+        canvas.addEventListener("touchmove", moving);
     };
 })();
